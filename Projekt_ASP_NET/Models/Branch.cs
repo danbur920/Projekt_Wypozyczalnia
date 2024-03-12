303469aef0ed567a4f1c9d964a336e6c419da1a6
danbur920 <116095790+danbur920@users.noreply.github.com>
2024-03-12 15:40:53 +0100

    Available background

--- Projekt_ASP_NET/Models/Branch.cs
+++ Projekt_ASP_NET/Models/Branch.cs
@@ -3,7 +3,7 @@
     public class Branch
     {
         public int Id { get; set; }
-        public string Locality { get; set; }
+        public string? Locality { get; set; }
         public int? NumberOfVehicles { get; set; }
         public virtual List<Vehicle>? VehicleItems { get; set; }
     }
